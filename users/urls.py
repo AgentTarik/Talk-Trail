81efc51806d9265f0bcd3f05df99713e3be0704a
Tarik <agenttarik99672@gmail.com>
2024-07-24 13:11:41 -0300

    Logout Button

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import UserRegisterView, UserLoginView, FeedView, HomePageView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('feed/', FeedView.as_view(), name='feed'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
