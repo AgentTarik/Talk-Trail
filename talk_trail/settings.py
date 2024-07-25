8ba95a81ca8bc7532df789537bf974757c779e2a
Tarik <agenttarik99672@gmail.com>
2024-07-25 15:15:06 -0300

    ROOT test

--- talk_trail/settings.py
+++ talk_trail/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 
 # Amazon S3 settings
@@ -148,8 +148,9 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('talktrail')
 AWS_S3_REGION_NAME = os.environ.get('sa-east-1')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# Static and media files settings
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
+# media files settings
+#MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Using S3 for static and media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
