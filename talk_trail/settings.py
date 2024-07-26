525de4194dedbe37c70820cf689b6704c301a843
Tarik <agenttarik99672@gmail.com>
2024-07-26 01:27:45 -0300

    aws setting fix

--- talk_trail/settings.py
+++ talk_trail/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'talk_trail.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-''''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,12 +88,12 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+
+#DATABASES = {
+    #'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#}
 
 
 # Password validation
@@ -137,23 +137,26 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
 # Amazon S3 settings
-AWS_ACCESS_KEY_ID = os.environ.get('AKIAQE3ROWJYAMENTCEA')
-AWS_SECRET_ACCESS_KEY = os.environ.get('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')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('talktrail')
-AWS_S3_REGION_NAME = os.environ.get('sa-east-1')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_ACCESS_KEY_ID = 'AKIAQE3ROWJYAMENTCEA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'talktrail'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'sa-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 # media files settings
-#MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Using S3 for static and media files
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://talktrail.s3.sa-east-1.amazonaws.com/'
 
 
 
