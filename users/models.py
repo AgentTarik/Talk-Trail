55c7bff2ccdf294e4ab09182bc1e512246d5602a
Tarik <agenttarik99672@gmail.com>
2024-07-27 22:29:29 -0300

    fix create_superuser

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ import os
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password, email):
+    def create_user(self, username, password, email, **extra_fields):
         if not username:
             raise ValueError('The Username field must be set')
         user = self.model(username=username, email=email)
