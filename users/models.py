cf66d19c1ca67b8497acc516c6143cbf644d42d6
Tarik <agenttarik99672@gmail.com>
2024-07-27 22:55:36 -0300

    fix create_superuser

--- users/models.py
+++ users/models.py
@@ -18,6 +18,11 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
         return self.create_user(username, password, **extra_fields)
 
 
